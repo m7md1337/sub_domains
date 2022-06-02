7ab754a5c85a52650236dc6264c32fbf9efaaa11
m7md1337 <kingsncksa@gmail.com>
2022-06-02 11:13:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,46 @@ import sys
 import argparse
 import concurrent.futures
 import datetime
+import random
+import art
+from lxml.html import fromstring
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 customelivedomains = []  # if outdict exists will save the data here
 listdomains = []  # lists from security and archive
 livedomains = []  # live domain from port_scan
 outdict = {}  # option -o to insert port with value in dict ex  -o 80/http will > {"80":"http"}
+statuss = []
+class myexcgetout(Exception):
+    pass
 
+
+class bcolors:
+
+    def conv(id):
+        if str(id)[:1] == "2":# 2XX return green
+            return '\033[92m'
+        elif str(id)[:1] == "3": # 3XX return blue
+            return '\033[94m'
+        elif str(id)[:1] == "4": # 4XX return Magenta
+            return '\033[35m'
+        elif str(id)[:1] == "5": # 5XX return red
+            return '\033[91m'
+
+
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    def1 =  '\033[49m'
+def random_char(y):
+    return ''.join(random.choice("!@#$%^&*()_+{}<>?\"'") for x in range(y))
 
 def parser_error(errmsg):
     print("an error occurred> Usage: python " + sys.argv[0] + " [Options] use -h for help")
@@ -26,9 +60,83 @@ def parse_args():
     parser.add_argument('-d', '--domain', help="Domain name to ex google.com", required=True)
     parser.add_argument('-p', '--ports', help='Scan the found subdomains against specified tcp ports', default="80,443")
     parser.add_argument('-a', '--apikey', help='api key use for securitytrails', )
+    parser.add_argument('-l', '--listOfHost', help='add the list of subdomains to test function check the port valid if add them to the text file, the file format should be host.com in each line  ', )
     parser.add_argument('-o', '--output', help='customizing the result 80,http/443,https use slash between the port,protocol')
+    parser.add_argument('-t', '--thread',help='default thread 80 ',type=int,default=80)
+    parser.add_argument('-e', '--endpoints', help='this option will check the urls with small endpoints input should be file text ')
+    parser.add_argument('-f', '--followtheredirect', help='follow the redirect default True ', type=int, default=True)
+    parser.add_argument('-s', '--statuss', help='show the data if status in defaults 200,204,302,301,307,401,403,405,500 ', type=int, default=[200,204,302,301,307,401,403,405,500])
     return parser.parse_args()
 
+def thee(nameoffunc, ports ,fileoflistorlist,thread=80):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=thread) as executor:
+        domains = {executor.submit(nameoffunc, subdomains, ports): subdomains for subdomains in fileoflistorlist}
+        for future in concurrent.futures.as_completed(domains):
+            subdomins = domains[future]
+            try:
+                data = future.result()
+            except Exception as exc:
+                pass
+
+
+def thee1(nameoffunc,url ,fileoflistorlist,followtheredirect):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=80) as executor:
+        domains = {executor.submit(nameoffunc, url, subdomains, followtheredirect): subdomains for subdomains in fileoflistorlist}
+        try:
+                def too():
+                    for future in concurrent.futures.as_completed(domains):
+                        subdomins = domains[future]
+                        #print(url+"/"+subdomins)
+                        print(f"{bcolors.def1}",">>> trying {}% ".format(subdomins[:10]), end='\r\r', flush=True)
+                        try:
+                            data = future.result()
+                            print("".join(map(str, data)),sep="")
+                        except Exception as e:
+                            #print(str(e))
+                            pass
+                too()
+        except KeyboardInterrupt:
+                print(f"{bcolors.def1}\nwait... {bcolors.ENDC}")
+                cc = input("\nplease enter first letter to move_to_next_url (m) kill_the_script (k) |||XXX|||no continue option for now|||XXX|||: ")
+                if cc == "m":
+                    print("moving ....")
+                    executor.shutdown(cancel_futures=True)
+                if cc == "k":
+                    print("killing the script ....")
+                    executor.shutdown(cancel_futures=True)
+                    raise myexcgetout()
+#                if cc == "c":
+#                    return ""
+
+def ss(url, endpoints, follow):
+        if endpoints[0] != "/":
+            endpoints = "/" + endpoints
+        try:
+            re = requests.get(url + endpoints, verify=False, timeout=5, allow_redirects=follow)
+            if re.status_code in statuss:
+                if len(re.text) != 0:
+                    tree = fromstring(re.content)
+                    if tree.findtext('.//title') is not None:
+                        return f"{bcolors.conv(re.status_code)}", "originUrl: " + url + endpoints + " ,TheRedirectUrl: " + re.url if follow else "URl: " + re.url, " ,titel: ", tree.findtext(
+                            './/title').replace("\n", "").replace("\r",
+                                                                  ""), " ,status: ", re.status_code, " ,redirect to :" + \
+                                                                                                     re.headers[
+                                                                                                         'Location'] + {
+                                                                                                         bcolors.ENDC} if "location" in re.headers else f"{bcolors.ENDC} "
+                    else:
+                        return f"{bcolors.conv(re.status_code)}", "from originUrl: " + url + endpoints + " ,To TheRedirectUrl: " + re.url if follow else "URl: " + re.url, " ,status: ", re.status_code, " ,redirect to :" + \
+                                                                                                                                                                                                         re.headers[
+                                                                                                                                                                                                             'Location'] + {
+                                                                                                                                                                                                             bcolors.ENDC} if "location" in re.headers else f"{bcolors.ENDC} "
+                else:
+                    return f"{bcolors.conv(re.status_code)}cool: ", url + endpoints, "too: ", re.url, "status:", re.status_code, "redirect to :" + \
+                                                                                                                                 re.headers[
+                                                                                                                                     'Location'] if "location" in re.headers else f"{bcolors.ENDC}"
+            else:
+                pass
+        except Exception as e:
+            # print(str(e))
+            pass
 
 def port_scan(host, ports):
     global livedomains, customelivedomains
@@ -50,8 +158,7 @@ def port_scan(host, ports):
             s.close()  # may occur an error before line 45 so ensure the socket is close
             pass
     if len(openports) > 0:
-        print("{}- Found open ports:{}".format(host, ', '.join(
-            str(v) for v in openports)))  # loop for data in array with modify the port to str
+        print(f"{bcolors.OKBLUE}","{} - Found open ports: ".format(host),f"{bcolors.FAIL}","{}".format(', '.join(str(v) for v in openports)) ,f"{bcolors.ENDC}",sep="")  # loop for data in array with modify the port to str
 
 
 # add to list
@@ -81,7 +188,7 @@ def byarchive(domain):
 def bysecuritytrails(domain, apikey):
     re = requests.get("https://api.securitytrails.com/v1/ping", headers={"APIKEY": apikey})
     if re.status_code != 200:
-        print("make sure you're use valid securitytrails api key the tools work without securitytrails api result")
+        print(f"{bcolors.WARNING}","error in api key for securitytrails : make sure you're use valid securitytrails api key the tools work without securitytrails api result",f"{bcolors.ENDC}",sep="")
         return ""
     try:
         url = 'https://api.securitytrails.com/v1/domain/{}/subdomains?children_only=false&include_inactive=true'.format(
@@ -94,14 +201,27 @@ def bysecuritytrails(domain, apikey):
 
 
 def main(): # define args
+    global statuss
     args = parse_args()
     domain = args.domain
     apikey = args.apikey
     savefile = args.output
     ports = args.ports
+    filefrominput = args.listOfHost
+    threads = args.thread
+    endpoints = args.endpoints
+    followtheredirect = args.followtheredirect
+    statuss = args.statuss
     ports = ports.split(',')
-    print("the result may take a time")
+    print(f"{bcolors.OKBLUE}the result may take a time",f"{bcolors.ENDC}",sep="")
     fiee = "result will save in file {domain}-data.txt in format host:port to customize the result use -o methode" # if -o option not use will be
+    if filefrominput:
+        try:
+            d = open(filefrominput, "r")
+            for xx in d.read().splitlines():
+                listdomains.append(xx)
+        except Exception:
+            exit("error : no file called {}".format(filefrominput))
     try:
         if savefile:
             fiee = "result will in file {domain}-data.txt" # if -o option used
@@ -109,38 +229,80 @@ def main(): # define args
             for xx in dd:
                 outdict[xx[0]] = xx[1] # add to outdict
     except Exception:
-        parser_error("error")
-    print(fiee)
+        exit("error")
+    print(f"{bcolors.OKBLUE}",fiee,f"{bcolors.ENDC}",sep="")
     if apikey != None:
         bysecuritytrails(domain, apikey)
         byarchive(domain)
     else:
         byarchive(domain)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        domains = {executor.submit(port_scan, subdomains, ports): subdomains for subdomains in listdomains}
-        for future in concurrent.futures.as_completed(domains):
-            subdomins = domains[future]
-            try:
-                data = future.result()
-            except Exception as exc:
-                pass
+    listdomains.sort()
+    thee(port_scan,ports,listdomains,thread=threads)
     if savefile:
         if customelivedomains:
-            name = "custom-{}-{}.txt".format(domain, datetime.datetime.now().strftime("%Y-%m-%d+%H:%M:%S"))
+            customelivedomains.sort()
+            name = "custom-{}-{}.txt".format(domain, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             file = open(name, "+a")
             for xx in customelivedomains:
                 file.write(xx + "\n")
             file.close()
-            print("custom file saved with name: " + name)
+            print(f"{bcolors.WARNING}custom file saved with name: " + name,f"{bcolors.ENDC}",sep="")
     name = "{}-{}.txt".format(domain, datetime.datetime.now().strftime("%Y-%m-%d+%H:%M:%S"))
     if livedomains:
+        livedomains.sort()
         file = open(name, "+a")
         for xx in livedomains:
             file.write(xx + "\n")
         file.close()
-        print("valid domain saved with name: " + name)
+        print(f"{bcolors.WARNING}valid domain saved with name: " + name,sep="")
     if not livedomains and not customelivedomains:
-        print("no data found for : {}".format(domain))
-
+        print(f"{bcolors.FAIL}","no data found for : {}".format(domain),f"{bcolors.ENDC}",sep="")
+    if endpoints and customelivedomains:
+        try:
+            ee = open(endpoints, "+r").read().splitlines()
+        except Exception:
+            exit("error : no file called {}".format(filefrominput))
+        input(f"{bcolors.OKBLUE}[to jump to next url enter Ctrl + c , to stop enter ctrl + c two times  ] -- please enter Enter to start {bcolors.ENDC}")
+        for xx in customelivedomains:
+            try:
+                print(f"{bcolors.def1}starting on url : " + xx + "\n "f"{bcolors.ENDC}")
+                thee1(ss, xx, ee,followtheredirect)
+            except myexcgetout:
+                print("see you soon")
+                break
 if __name__ == '__main__':
+    # print(f"{bcolors.OKBLUE}")
+    # print(random_char(32)+" "*20+random_char(38))
+    # print(random_char(32) + " " * 20 + random_char(39))
+    # print(random_char(32) + " " * 20 + random_char(40))
+    # print(random_char(32) + " " * 20 + random_char(12)+" "*20+random_char(12))
+    # print(random_char(12) + " " * 40 + random_char(12) + " " * 20 + random_char(11))
+    # print(random_char(12) + " " * 40 + random_char(12) + " " * 20 + random_char(12))
+    # print(random_char(12) + " " * 40 + random_char(12) + " " * 20 + random_char(11))
+    # print(random_char(12) + " " * 40 + random_char(12) + " " * 20 + random_char(11))
+    # print(random_char(12) + " " * 40 + random_char(12) + " " * 20 + random_char(11))
+    # print(random_char(12) + " " * 40 + random_char(12) + " " * 20 + random_char(11))
+    # print(random_char(12) + " " * 40 + random_char(12) + " " * 20 + random_char(11))
+    # print(random_char(12) + " " * 40 + random_char(12) + " " * 20 + random_char(11))
+    # print(random_char(12) + " " * 40 + random_char(12) + " " * 20 + random_char(11))
+    # print(random_char(32) + " " * 20 + random_char(12) + " " * 20 + random_char(11))
+    # print(random_char(32) + " " * 20 + random_char(12) + " " * 20 + random_char(11))
+    # print(" "*20+random_char(12) + " " * 20 + random_char(12) + " " * 20 + random_char(11))
+    # print(" " * 20 + random_char(12) + " " * 20 + random_char(12) + " " * 20 + random_char(11))
+    # print(" " * 20 + random_char(12) + " " * 20 + random_char(12) + " " * 20 + random_char(11))
+    # print(" " * 20 + random_char(12) + " " * 20 + random_char(12) + " " * 20 + random_char(11))
+    # print(" " * 20 + random_char(12) + " " * 20 + random_char(12) + " " * 20 + random_char(11))
+    # print(" " * 20 + random_char(12) + " " * 20 + random_char(12) + " " * 20 + random_char(11))
+    # print(" " * 20 + random_char(12) + " " * 20 + random_char(12) + " " * 20 + random_char(11))
+    # print(" " * 20 + random_char(12) + " " * 20 + random_char(12) + " " * 20 + random_char(10))
+    # print(" " * 20 + random_char(12) + " " * 20 + random_char(12) + " " * 20 + random_char(11))
+    # print(random_char(32) + " " * 20 + random_char(12)+" "*20+random_char(12))
+    # print(random_char(32) + " " * 20 + random_char(40))
+    # print(random_char(32) + " " * 20 + random_char(39))
+    # print(random_char(32)+" "*20+random_char(38))
+    # print(f"{bcolors.ENDC}")
+    print(f"{bcolors.OKBLUE}")
+    print(art.text2art("sub_DOMAINS","rand"))
+    print(f"{bcolors.ENDC}")
+
     main()
